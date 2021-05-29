a845b706d890556e7c0c555276f0fff8a0d8088f
davidcwh <dcwh12321@gmail.com>
2021-05-30 01:51:24 +0800

    Add copy url button

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
-import { useState } from 'react';
-import { Col, Container, Button, Form } from 'react-bootstrap';
+import { useEffect, useState } from 'react';
+import { Col, Container, Button, Form, Alert } from 'react-bootstrap';
 import axios from 'axios';
 
 function App() {
     const [ text, setText ] = useState('');
+    const [isShortenUrl, setIsShortenUrl] = useState(false);
+    const [isTextCopied, setIsTextCopied] = useState(false);
+    const [showError, setShowError] = useState(false);
+    const [errorMessage, setErrorMessage] = useState('');
 
     const createShortUrl = (url) => {
         const params = new URLSearchParams();
@@ -18,11 +22,53 @@ function App() {
 
         axios.post('http://localhost:3000/createShortUrl', params, config)
         .then(response => {
-            setText(response.data.shortUrl)
+            setText(response.data.shortUrl);
+            setIsShortenUrl(true);
         }, error => {
+            setShowError(true);
             console.log(error);
         });
-    }
+    };
+
+    const handleTextChange = (text) => {
+        setText(text);
+        setIsShortenUrl(false);
+    };
+
+    const copyTextToClipboard = (text) => {
+        navigator.clipboard.writeText(text).then(function() {
+            setIsTextCopied(true);
+            console.log('Async: Copying to clipboard was successful!');
+        }, function(err) {
+        console.error('Async: Could not copy text: ', err);
+        });
+    };
+
+    // Refer button from "Copied!" to "Copy" after 1 second upon click
+    useEffect(() => {
+        if(isTextCopied) {
+            const timer = setTimeout(() => {
+                setIsTextCopied(false);
+            }, 1000);
+
+            return () => {
+                clearTimeout(timer);
+            } 
+        }
+    }, [isTextCopied]);
+
+    // Hide error message after 3 seconds
+    useEffect(() => {
+        if(showError) {
+            const timer = setTimeout(() => {
+                setShowError(false);
+            }, 3000);
+
+            return () => {
+                clearTimeout(timer);
+            } 
+        }
+    }, [showError]);
 
     return (
         <div className="App">
@@ -33,23 +79,38 @@ function App() {
                         <Form.Row>
                             <Col>
                                 <Form.Control 
+                                    className="shorten-url-text"
+                                    id="urlText"
                                     placeholder="Shorten your link" 
                                     size="lg"
                                     value={text}
-                                    onChange={e => setText(e.target.value)}
+                                    onChange={e => handleTextChange(e.target.value)}
                                 />
                             </Col>
                             <Col sm="auto"> 
+                                {isShortenUrl ? 
                                 <Button 
+                                    bsClass="copy-button"
+                                    variant={isTextCopied ? "success" : "primary"} 
+                                    size="lg"
+                                    onClick={() => copyTextToClipboard(text)}>
+                                    {isTextCopied ? "Copied!" :"Copy"}
+                                </Button>
+                                : <Button 
                                     variant="primary" 
                                     size="lg"
                                     onClick={() => createShortUrl(text)}
+                                    disabled={text === ''}
                                 >
                                     Shorten
                                 </Button>
+                                }
                             </Col>
                         </Form.Row>
                     </Form>
+                    {showError && <Alert variant="danger" >
+                                    <p>Unable to shorten that link</p>
+                                </Alert>}
                 </Container>
             </header>
         </div>
