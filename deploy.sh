24405ff971604fed2720d402d4226f8a5c416bf2
davidcwh <dcwh12321@gmail.com>
2021-06-18 16:33:45 +0800

    Change to demo directory on droplet

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,12 @@
 set -xe
 
 if [ $TRAVIS_BRANCH == 'master' ] ; then
+  echo "Deploying to droplpet"
   eval "$(ssh-agent -s)"
   ssh-add ~/.ssh/id_rsa
 
-  rsync -a --exclude={'/node_modules','/src','/public'} client/ david@139.59.216.92:/home/david/friendly-link/client
-  rsync -a server/ david@139.59.216.92:/home/david/friendly-link/server
+  rsync -a --exclude={'/node_modules','/src','/public'} client/ david@139.59.216.92:/home/david/demo/client
+  rsync -a server/ david@139.59.216.92:/home/david/demo/server
 else
   echo "Not deploying, since the branch isn't master."
 fi
