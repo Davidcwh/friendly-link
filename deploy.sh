4c9fedfd22eb37dae52529aafd17c945584029ed
davidcwh <dcwh12321@gmail.com>
2021-06-18 16:05:40 +0800

    Change droplet user in bash script

--- deploy.sh
+++ deploy.sh
@@ -4,8 +4,8 @@ if [ $TRAVIS_BRANCH == 'master' ] ; then
   eval "$(ssh-agent -s)"
   ssh-add ~/.ssh/id_rsa
 
-  rsync -a --exclude={'/node_modules','/src','/public'} client/ travis@139.59.216.92:/home/david/friendly-link/client
-  rsync -a server/ travis@139.59.216.92:/home/david/friendly-link/server
+  rsync -a --exclude={'/node_modules','/src','/public'} client/ david@139.59.216.92:/home/david/friendly-link/client
+  rsync -a server/ david@139.59.216.92:/home/david/friendly-link/server
 else
   echo "Not deploying, since the branch isn't master."
 fi
