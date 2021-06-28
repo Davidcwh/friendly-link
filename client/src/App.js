dcdb8177c6653422717aacb259579598781c756d
davidcwh <dcwh12321@gmail.com>
2021-06-28 20:52:34 +0800

    Add protected-route

--- client/src/App.js
+++ client/src/App.js
@@ -5,18 +5,15 @@ import { useAuth0 } from '@auth0/auth0-react';
 import LandingPage from './pages/LandingPage';
 import UserDashboardPage from './pages/UserDashboardPage';
 import LoadingPage from './pages/LoadingPage';
+import ProtectedRoute from './auth/protected-route';
 
 function App() {
     const { isLoading } = useAuth0();
 
     return (
         <Switch>
-            <Route exact path="/">
-                {isLoading ? <LoadingPage/> : <LandingPage/>}
-            </Route>
-            <Route exact path="/dashboard">
-                <UserDashboardPage/>
-            </Route>
+            <Route exact path="/" component={isLoading ? LoadingPage : LandingPage}/>
+            <ProtectedRoute exact path="/dashboard" component={UserDashboardPage}/>
         </Switch>
     );
 }
