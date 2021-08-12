dbfe88812cc1aa1a9d35aa70bdaee06b6f9c5f4a
davidcwh <dcwh12321@gmail.com>
2021-08-12 21:13:12 +0800

    update deploy client location

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ if [ $TRAVIS_BRANCH == 'main' ] ; then
   eval "$(ssh-agent -s)"
   ssh-add ~/.ssh/id_rsa
 
-  rsync -a --exclude={'/node_modules','/src','/public'} client/ david@139.59.216.92:/home/david/demo/client
+  rsync -a --exclude={'/node_modules','/src','/public'} client/build/ david@139.59.216.92:/var/www/frnd.link/html
   rsync -a server/ david@139.59.216.92:/home/david/demo/server
 
   ssh david@139.59.216.92 'pm2 restart all'
